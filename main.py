b1022418a7efc35e2371c1313d1cf86286cfd3e8
Yevhen Mazur <yevhen.mazur@pm.me>
2024-05-23 19:14:31 +0200

    Add command autocompillation

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from _function.save_load_data import save_data, load_data
 from _function.show import show_phone, show_all, show_birthday, birthdays
 
 from prompt_toolkit import prompt
-from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.completion import WordCompleter,Completer, Completion
 from rich.console import Console
 from rich.prompt import Prompt
 
@@ -46,6 +46,15 @@ completer = WordCompleter(commands, ignore_case=True)
 
 console = Console()
 
+class FirstWordCompleter(Completer):
+    def __init__(self, completer):
+        self.completer = completer
+
+    def get_completions(self, document, complete_event):
+        # Якщо курсор знаходиться на першому слові, використовувати completer
+        if document.cursor_position_row == 0 and len(document.text_before_cursor.split()) <= 1:
+            yield from self.completer.get_completions(document, complete_event)
+
 def main():
     book = load_data(filename=f"{data_path}/addressbook.pkl", class_name="AddressBook")
     notebook = load_data(filename=f"{data_path}/notebook.pkl", class_name="NoteBook")
@@ -54,71 +63,75 @@ def main():
 
     while True:
         # user_input = input("Enter a command: ")
-        user_input = prompt('>>> ', completer=completer)
-        command, *args = parse_input(user_input)
-
-        if command in ["close", "exit"]:
+        user_input = prompt('>>> ', completer=FirstWordCompleter(completer))
+        # command, *args = parse_input(user_input)
+        command = user_input.split()
+        
+        if not command:
+                continue
+        
+        if command[0] in ["close", "exit"]:
             save_data(book, filename=f"{data_path}/addressbook.pkl")
             save_data(notebook, filename=f"{data_path}/notebook.pkl")
             console.print("[red]Good bye![/red]")
             break
-        elif command == "hello":
+        elif command[0] == "hello":
             console.print("How can I help you?\n")
-        elif command == "add":
+        elif command[0] == "add":
             result = add_contact(args, book)
             console.print(f"{result}\n")
-        elif command == "change":
+        elif command[0] == "change":
             change_contact(args, book)
             console.print("Contact changed.\n")
-        elif command == "delete":
+        elif command[0] == "delete":
             delete_contact(args, book)
             console.print("Contact deleted.\n")
-        elif command == "show":
+        elif command[0] == "show":
             result = show_phone(book, *args)
             console.print(f"{result}\n")
-        elif command == "all":
+        elif command[0] == "all":
             result = show_all(book)
             if result:
                 console.print(f"{result}\n")
             else:
                 console.print("No contacts found.\n")
-        elif command == "add_birthday":
+        elif command[0] == "add_birthday":
             add_birthday(args, book)
             console.print("Birthday added.\n")
-        elif command == "change_birthday":
+        elif command[0] == "change_birthday":
             change_birthday(args, book)
             console.print("Birthday changed.\n")
-        elif command == "show_birthday":
+        elif command[0] == "show_birthday":
             result = show_birthday(args, book)
             console.print(f"Birthday: {result}\n")
-        elif command == "birthdays":
+        elif command[0] == "birthdays":
             result = birthdays(book)
             console.print(f"Upcoming birthdays: {result}\n")
         
-        elif command == "add-note":
+        elif command[0] == "add-note":
             add_note(args, notebook)
-        elif command == "delete-note":
+        elif command[0] == "delete-note":
             delete_note(args, notebook)
-        elif command == "list-notes":
+        elif command[0] == "list-notes":
             list_notes(notebook)
-        elif command == "show-note":
+        elif command[0] == "show-note":
             show_note(args, notebook)
-        elif command == "edit-note":
+        elif command[0] == "edit-note":
             edit_note(args, notebook)
         
-        elif command == "add-email":
+        elif command[0] == "add-email":
             add_email(book, *args)
             console.print("Email added.\n")
-        elif command == "change-email":
+        elif command[0] == "change-email":
             change_email(args, book)
             console.print("Email changed.\n")
-        elif command == "add-address":
+        elif command[0] == "add-address":
             add_address(args, book)
             console.print("Address added.\n")
-        elif command == "change-address":
+        elif command[0] == "change-address":
             change_address(args, book)
             console.print("Address changed.\n")
-        elif command == "help":
+        elif command[0] == "help":
             console.print(display_help())
         else:
             console.print("[red]Invalid command[/red].\n")
