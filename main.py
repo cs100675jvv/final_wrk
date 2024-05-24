213259017d95d95965206b0d131988e5a2124f67
Slava-Viacheslav Chukhno (Hitachi) <vchukhno@hitachisolutions.com>
2024-05-25 01:53:20 +0300

    encrypted saving

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # import tkinter as tk
 # from tkinter import font as tkfont
 # from _classes.notes import NoteBook
+import getpass
 from _function.add_contact import add_contact, add_birthday, add_email, add_address
 from _function.add_note import add_note
 from _function.list_notes import list_notes
@@ -13,7 +14,7 @@ from _function.delete_contact import delete_contact
 from _function.help import print_help
 from _function.find_note import find_note
 from _function.parse import parse_input
-from _function.save_load_data import save_data, load_data
+from _function.save_load_data import DataManager
 from _function.show import show_phone, show_all, show_birthday, birthdays
 
 from prompt_toolkit import prompt
@@ -21,6 +22,7 @@ from prompt_toolkit.completion import WordCompleter, Completer
 from rich.console import Console
 
 data_path = "./_files"
+data_file_name = "DataBundle"
 
 commands = [
     'close',
@@ -50,6 +52,7 @@ commands = [
 completer = WordCompleter(commands, ignore_case=True)
 
 console = Console()
+data_manager = DataManager(data_path)
 
 class FirstWordCompleter(Completer):
     def __init__(self, completer):
@@ -63,34 +66,61 @@ class FirstWordCompleter(Completer):
         yield from self.completer.get_completions(document, complete_event)
 
 def main():
-    book = load_data(filename=f"{data_path}/addressbook.pkl", class_name="AddressBook")
-    notebook = load_data(filename=f"{data_path}/notebook.pkl", class_name="NoteBook")
+    # loading data plain by default
+    data_bundle = data_manager.load_data_plain(data_file_name)
 
+    # loading refs to address_book and note_book
+    book = data_bundle.address_book
+    notebook = data_bundle.note_book
+
+    # changes_made flag
+    changes_made = False
+
+    # greeting
     print("Hello. How can I help you today?\n")
-
+    
+    # user interaction loop
     while True:
         user_input = prompt('>>> ', completer=FirstWordCompleter(completer))
         command, *args = parse_input(user_input)
         
         if not command:
                 continue
-        
-        if command in ["close", "exit"]:
-            save_data(book, filename=f"{data_path}/addressbook.pkl")
-            save_data(notebook, filename=f"{data_path}/notebook.pkl")
-            console.print("[red]Good bye![/red]")
+
+        # commands - saving and loading data, exiting
+        elif command == "save":
+            data_manager.save_data_plain(data_bundle, data_file_name)
+            changes_made = False
+        elif command == "save-secure":
+            data_manager.save_data_encrypted(data_bundle, data_file_name)
+            changes_made = False
+        elif command == "load":
+            data_manager.load_data_plain(data_file_name)
+            changes_made = False
+        elif command == "load-secure":
+            data_manager.load_data_encrypted(data_file_name)
+            changes_made = False
+        elif command in ["close", "exit"]:
+            if changes_made:
+                confirm = input("You have unsaved changes. Are you sure you want to exit? (yes/no) ")
+                if confirm.lower() != "yes":
+                    continue
+            print("[red]Good bye![/red]")
             break
-        elif command == "hello":
-            console.print("How can I help you?\n")
+
+        # commands - contact viewing and editing
         elif command == "add":
             result = add_contact(args, book)
             console.print(f"{result}\n")
+            changes_made = True
         elif command == "change":
             change_contact(args, book)
             console.print("Contact changed.\n")
+            changes_made = True
         elif command == "delete":
             delete_contact(args, book)
             console.print("Contact deleted.\n")
+            changes_made = True
         elif command == "show_phone":
             result = show_phone(book, *args)
             console.print(f"{result}\n")
@@ -100,11 +130,15 @@ def main():
                 console.print(f"{result}\n")
             else:
                 console.print("No contacts found.\n")
+
+        # commands - contact birthdays
         elif command == "add_birthday":
             add_birthday(args, book)
+            changes_made = True
             # console.print("Birthday added.\n")
         elif command == "change_birthday":
             change_birthday(args, book)
+            changes_made = True
             # console.print("Birthday changed.\n")
         elif command == "show_birthday":
             result = show_birthday(args, book)
@@ -112,32 +146,46 @@ def main():
         elif command == "birthdays":
             result = birthdays(book)
             console.print(f"Upcoming birthdays: {result}\n")
-        
+
+        # commands - notes viewing and editing
         elif command == "add_note":
             add_note(args, notebook)
+            changes_made = True
         elif command == "delete_note":
             delete_note(args, notebook)
+            changes_made = True
         elif command == "list_notes":
             list_notes(notebook)
         elif command == "show_note":
             show_note(args, notebook)
         elif command == "edit_note":
             edit_note(args, notebook)
+            changes_made = True
         elif command == "find_note":
             find_note(args, notebook)
-        
+            changes_made = True
+
+        # commands - email and address
         elif command == "add_email":
             add_email(book, *args)
+            changes_made = True
             # console.print("Email added.\n")
         elif command == "change_email":
             change_email(args, book)
+            changes_made = True
             # console.print("Email changed.\n")
         elif command == "add_address":
             add_address(args, book)
+            changes_made = True
             # console.print("Address added.\n")
         elif command == "change_address":
             change_address(args, book)
+            changes_made = True
             # console.print("Address changed.\n")
+
+        # commands - miscellaneous
+        elif command == "hello":
+            console.print("How can I help you?\n")
         elif command == "help":
             console.print(print_help())
         else:
