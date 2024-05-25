6c7432350c90f42fdedc962d63919a1d6c6d1653
cs100675jvv <cs100675jvv@gmail.com>
2024-05-25 19:47:43 +0300

    correct isue with empty birthday

--- _function/find_note.py
+++ _function/find_note.py
@@ -5,18 +5,18 @@ def find_note(args, notebook: NoteBook) -> Note:
     substring = ' '.join(args)
     headers = ["ID", "Header", "Body", "Creation Date"]
     rows = []
+    
     for note in notebook.values():
         if substring in note.body:
             note_id = str(note.id)
             note_header = note.header 
             note_body = note.body 
             note_creation = note.creation_date.strftime('%Y-%m-%d %H:%M:%S')
-            row = [note_id, note_header, note_body, note_creation].join(',')
+            row = [note_id, note_header, note_body, note_creation]
             rows.append(row)
-            data = [rows.append(row)]
-        if rows:
-            col_widths = [15, 15, 60, 15]
-            return format_table(headers, data, col_widths)
-        else:
-            return "No notes found containing the given substring."
+    if rows:
+        col_widths = [15, 15, 60, 15]
+        return format_table(headers, rows, col_widths)
+    else:
+        return "No notes found containing the given substring."
 
